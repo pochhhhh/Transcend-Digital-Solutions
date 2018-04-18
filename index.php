907db5af6f275dd013c22e197334ce2175a3a5cc
Marlon <marlonpempengco@gmail.com>
2018-04-18 10:06:17 +0100

    Set up initial files

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Transcend Digital Solutions</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+<link>
+
+
+</head>
+
+<body>
+
+
+
+
+
+
+<h1 id="test">Generic Hello World Message </h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="scripts.js"></script>
+</body>
+
+</html>
